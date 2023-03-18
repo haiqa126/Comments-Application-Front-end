dd1617ed2d6474e16b68275f800f53a1ab874b80
Abeer waseem <abeer@pacandcopy.com>
2023-03-18 15:53:25 +0000

    bug fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import "./App.css";
 import React from "react";
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 import Feedback from "./Components/Feedback";
 import Signin from "./Components/Signin";
 import About from "./Components/About";
 import Routes from "./routes";
 
 function App() {
-  return (
+  return >
     <>
-      {/* <Feedback /> */}
-
-      <Routes />
-      {/* <About /> */}
+      <div className="App">
+        <Router>
+          <Header></Header>
+          <div className="container">
+          <Routes>
+              <Route path="/" element={<Signin />} />
+              <Route path="/about" element={<About />} />
+          </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </div>
     </>
   );
 }
